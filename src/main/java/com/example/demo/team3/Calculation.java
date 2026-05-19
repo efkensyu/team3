4cc494281ba775bdf30475756b20a21fc920219e
ogawa.t <tom_ogawa@bold.ne.jp>
2026-05-19 17:56:47 +0900

    0519作業ファイル追加

---
+++ src/main/java/com/example/demo/team3/Calculation.java
@@ -0,0 +1,7 @@
+package com.example.demo.team3;
+
+public class Calculation {
+
+	//計算メソッドを追加してください。
+
+}
---
+++ src/main/java/com/example/demo/team3/HouseController.java
@@ -0,0 +1,10 @@
+package com.example.demo.team3;
+
+public class HouseController {
+
+	public static void main(String[] args) {
+		// Returnと@Contoroller使って画面遷移させてください。
+
+	}
+
+}
