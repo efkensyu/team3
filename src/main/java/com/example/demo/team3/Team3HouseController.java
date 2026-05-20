55e0d61e20bf4fd2be0a60bb6f3dc0beb9c41f9a
hamaguchi <t_hamaguchi@bold.ne.jp>
2026-05-20 16:34:25 +0900

    Revert "0520-OT-KakeiboContorollerにRequiredArgsConstructor追加"
    This reverts commit a4aa36a1a18a9986bb3bd0236ccc949cf6ea85b9.

--- src/main/java/com/example/demo/team3/Team3HouseController.java
+++ src/main/java/com/example/demo/team3/Team3HouseController.java
@@ -1,18 +1,10 @@
 package com.example.demo.team3;
 
-import org.springframework.stereotype.Controller;
-
-import com.example.demo.team3.Team3Repository.Team3Repository;
-
-import lombok.RequiredArgsConstructor;
-
-
-@RequiredArgsConstructor
-@Controller	
 public class Team3HouseController {
-	
-	private final Team3Repository repository;
-	// Returnと@Contoroller使って画面遷移させてください。
-	
+
+	public static void main(String[] args) {
+		// Returnと@Contoroller使って画面遷移させてください。
+
+	}
 
 }
--- src/main/java/com/example/demo/team3/Team3Repository/Team3Repository.java
+++ src/main/java/com/example/demo/team3/Team3Repository/Team3Repository.java
@@ -1,11 +1,12 @@
 package com.example.demo.team3.Team3Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.team3.Team3Entity.KakeiboEntity;
 
-@Repository
-public interface Team3Repository extends JpaRepository<KakeiboEntity, Integer>{
+public class Team3Repository {
+	public interface KakeiboRepository extends JpaRepository<KakeiboEntity, Integer> {
+
+	}
 
 }
