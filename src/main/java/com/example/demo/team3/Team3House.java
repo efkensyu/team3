47c0b7a34e84d6243c5f08c2218711dafc439164
koshiishi <m_koshiishi@bold.ne.jp>
2026-05-20 16:24:03 +0900

    0520-KM-本日分最終登録

---
+++ src/main/java/com/example/demo/team3/Team3House.java
@@ -0,0 +1,16 @@
+package com.example.demo.team3;
+
+public class Team3House {
+	private String name;
+	
+	private int price;
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+}
