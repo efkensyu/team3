28f9d24461bf4156c63a3f1412fc5c01beac6d06
hamaguchi <t_hamaguchi@bold.ne.jp>
2026-05-20 10:24:29 +0900

    0520-HT-カレンダー画面の追加

---
+++ src/main/java/com/example/demo/team3/Team3Kalender.java
@@ -0,0 +1,23 @@
+package com.example.demo.team3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Team3Kalender {
+	
+	
+	@GetMapping("/kalender")
+		public String index() {
+			return "Team3KalenderIn";
+		}
+	
+	@PostMapping("/kalender")
+		public String send(@RequestParam("day") String day,Model model) {
+		model.addAttribute("day",day);
+		return "Team3KalenderOut";
+	}
+}
