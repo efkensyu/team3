a39d66bd7392f47cfe28353c8b8cb81be3c796c6
ogawa.t <tom_ogawa@bold.ne.jp>
2026-05-21 11:46:40 +0900

    Revert "0521-OT-ミスった"
    This reverts commit 8f1e3e9f6d485b07a48fb6fc5f21296b5f6670bd.

--- src/main/java/com/example/demo/team3/Team3HouseController.java
+++ src/main/java/com/example/demo/team3/Team3HouseController.java
@@ -7,10 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.RequiredArgsConstructor;
-@SessionAttributes({"team3House","ary"})
+@SessionAttributes("team3House")
 @RequiredArgsConstructor
 @Controller	
 public class Team3HouseController {
@@ -18,7 +19,14 @@ public class Team3HouseController {
 	//private final Team3Repository repository;
 ArrayList<Team3House> ary = new ArrayList<>();
 	
-
+//	//Todo2というHTMLファイルを探す
+//	@GetMapping("/controller")
+//	public String start(Model model) {
+//		model.addAttribute("ary", ary);
+//		return "/Team3displayOut";
+//	}
+	
+	//追加ボタンを押したら記入内容が同ファイルにとぶ
 	@GetMapping("/Team3displayOut")
 	public String get(Model model) {
 		model.addAttribute("team3House", new Team3House());
@@ -26,41 +34,33 @@ ArrayList<Team3House> ary = new ArrayList<>();
 		return "/Team3displayOut";
 	}
 	
-	//追加
 	@PostMapping("/add")
-	public String add(
-			@ModelAttribute Team3House team3House ,
-			@ModelAttribute("ary") ArrayList<Team3House> ary) {
+	public String add(@ModelAttribute Team3House team3House) {
 		ary.add(team3House);
 		return "redirect:/Team3displayOut";
 	}
 	
-	//削除
-//	@PostMapping("/delete")
-//	public String delete(@RequestParam("index")int index) {
-//		ary.remove(index);
-//		return "redirect:/Team3displayOut";
-//	}
-//	
-//	//更新
-//	@PostMapping("/update")
-//	public String update(
-//					@ModelAttribute("team3House") Team3House team3House ,
-//					@RequestParam("index") int index) {
-//		ary.set(index, team3House);
-//		return "redirect:/Team3displayOut";
-//	}
+	@PostMapping("/delete")
+	public String delete(@RequestParam("index")int index) {
+		ary.remove(index);
+		return "redirect:/Team3displayOut";
+	}
 	
-	//カレンダーに戻る
+	@PostMapping("/update")
+	public String update(
+					@ModelAttribute("team3House") Team3House team3House ,
+					@RequestParam("index") int index) {
+		ary.set(index, team3House);
+		return "redirect:/Team3displayOut";
+	}
+	
 	@PostMapping("/cancel")
 	public String cancel() {
 		return "/Team3KalenderIn";
 	}
 	
-	//displayin
 	@PostMapping("/save")
 	public String save(@ModelAttribute Team3House team3House, Model model) {
-		model.addAttribute("ary",ary);
 		model.addAttribute("team3House", team3House);
 		return "/Team3displayIn";
 	}
