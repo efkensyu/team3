c0b953493c71ff0fbe198bd07d5fda8b3d5c9a24
koshiishi <m_koshiishi@bold.ne.jp>
2026-05-21 10:29:55 +0900

    0521-KM-revertする用

--- src/main/java/com/example/demo/team3/Team3House.java
+++ src/main/java/com/example/demo/team3/Team3House.java
@@ -2,22 +2,21 @@ package com.example.demo.team3;
 
 public class Team3House {
 	private String name;
-	
 	private int price;
 
-	public String getName() {
-		return name;
-	}
-
-	public int getPrice() {
-		return price;
-	}
-	
-	public void setName(String name) { 
-		this.name = name;
-	}
-	
-    public void setPrice(int price) { 
-    		this.price = price; 
-    	}
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public int getPrice() {
+//		return price;
+//	}
+//	
+//	public void setName(String name) { 
+//		this.name = name;
+//	}
+//	
+//    public void setPrice(int price) { 
+//    		this.price = price; 
+//    	}
 }
