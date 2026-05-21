d39f722fb0b1fdbd9c6d8ea3ce8ecb3872d11307
ogawa.t <tom_ogawa@bold.ne.jp>
2026-05-21 14:50:26 +0900

    0521-OT-練習

--- src/main/java/com/example/demo/team3/Team3Kalender.java
+++ src/main/java/com/example/demo/team3/Team3Kalender.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+@SessionAttributes("day")
 @Controller
 public class Team3Kalender {
 	
