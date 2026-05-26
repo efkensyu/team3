6f9cf8c5e6c9ef3c0d928aa375316e426c9f148b
koshiishi <m_koshiishi@bold.ne.jp>
2026-05-26 09:28:34 +0900

    Revert "0526-Km-validationできなかったのでrevertする"
    This reverts commit b768f1ecb72f434ed0a51b37612acb8492619e7e.

--- src/main/java/com/example/demo/team3/Team3KalenderController.java
+++ src/main/java/com/example/demo/team3/Team3KalenderController.java
@@ -3,14 +3,10 @@ package com.example.demo.team3;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
-import com.example.demo.team3.Team3Entity.Team3DayEntity;
 
 @SessionAttributes("day")
 @Controller
@@ -18,28 +14,22 @@ public class Team3KalenderController {
 	
 	
 	@GetMapping("/Team3kalender")
-		public String index(Model model) {
-		
-			if (!model.containsAttribute("team3DayEntity")) {
-	            model.addAttribute("team3DayEntity", new Team3DayEntity());
-	        }
+		public String index() {
 			
 			return "team3/Team3KalenderIn";
 		}
 	
 	@PostMapping("/Team3kalender")
-		public String send(@ModelAttribute("team3DayEntity") 
-		@Validated Team3DayEntity form,
-	            BindingResult result,
-	            Model model) {
+		public String send(@RequestParam("day") String day,Model model) {
+		try {
+			model.addAttribute("day",day);
+		}catch(Exception e) {
+			 e.printStackTrace();
+			 return "team3/Error";
+		}
 		
-        if (result.hasErrors()) {
-            return "team3/Team3KalenderIn";
-        }
-
-        model.addAttribute("day", form.getDay());
-        return "team3/Team3displayIn";
-    }
+		return "team3/Team3displayIn";
+	}
 		
 	@PostMapping("/Team3cancel1")
 	public String cancel1() {
