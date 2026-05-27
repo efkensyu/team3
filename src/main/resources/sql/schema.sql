9586e7d9f0276aadd7c52158250b80091db614fd
koshiishi <m_koshiishi@bold.ne.jp>
2026-05-27 13:39:47 +0900

    0527-KM-CSS修正

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE login_tbl (
     username VARCHAR(100) UNIQUE NOT NULL
 --    password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE team3_syohin_tbl (
+ id INT PRIMARY KEY AUTO_INCREMENT,
+ date DATE NOT NULL,
+ category VARCHAR(100) NOT NULL,
+ price INT NOT NULL,
+ amount INT NOT NULL,
+ result INT AS (price * amount)
+);
